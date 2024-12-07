b72d9d513817203edde1fe14935bc1298eef46fd
Masataka Hisasue <masataka.hisasue@gmail.com>
2024-12-07 15:53:58 +0900

    Feat text component

---
+++ lib/components/text.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class BodyText extends StatelessWidget {
+  const BodyText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: const Text(
+        'コンポーネント',
+      ),
+    );
+  }
+}
